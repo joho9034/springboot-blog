94ee2b0eb81c1d3f96009809867ff2e2659237a7
이재학 <joho9034@empal.com>
2021-07-15 18:03:56 +0900

    entity 작성

---
+++ src/main/java/com/hak/blog/model/Board.java
@@ -0,0 +1,51 @@
+package com.hak.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.hak.blog.model.User.UserBuilder;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Board {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Lob
+	private String content;
+	
+	@ColumnDefault("0")
+	private int count;
+	
+	@ManyToOne // Many = Board, One = User
+	@JoinColumn(name = "userId")
+	private User user;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+	
+}
